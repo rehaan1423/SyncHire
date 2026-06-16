a79a643e563a272a5e954e60d6bdb4ab56fc1e3f
rehaan1423 <mdrehaanlodhia@gmail.com>
2026-06-16 13:55:22 +0530

    Setting up for intial deployment

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import path from "path"
+
+const __dirname = path.resolve();
 
 const app  = express();
 const port = ENV.PORT;
+
+app.get("/test",(req,res) => res.send("tested"));
+app.get("/test3",(req,res) => res.send("tested3"));
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("{*any}",(req,res) =>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    });
+}
 
 app.listen(port,()=>{
     console.log(`Server Running on port ${port}`);
