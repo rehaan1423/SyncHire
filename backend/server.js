46511f7c44359bb202f6b9f8195a5bb3dc294c21
rehaan1423 <mdrehaanlodhia@gmail.com>
2026-06-19 17:44:52 +0530

    WebHooks and initial chat api

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,45 @@
-import express from "express";
-import { ENV } from "./lib/env.js";
-import path from "path"
+import dns from "node:dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
+import express from "express";
+import cors from "cors";
+import { ENV } from "./src/lib/env.js";
+import path from "path"
+import { connectDB } from "./src//lib/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import chatRoutes from "./src/routes/chatRoutes.js";
+import webhookRoutes from "./src/routes/webhookRoutes.js";
+
+const app = express();
+const port = ENV.PORT;
 const __dirname = path.resolve();
 
-const app  = express();
-const port = ENV.PORT;
+app.use(
+  "/api/webhooks/clerk",
+  express.raw({ type: "application/json" }),
+  webhookRoutes
+);
 
-app.get("/test",(req,res) => res.send("tested"));
-app.get("/test3",(req,res) => res.send("tested3"));
+app.use(express.json());
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+app.use(clerkMiddleware());
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+app.get("/test", (req, res) => res.send("tested"));
+app.get("/test3", (req, res) => res.send("tested3"));
 
-    app.get("{*any}",(req,res) =>{
-        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     });
 }
 
-app.listen(port,()=>{
-    console.log(`Server Running on port ${port}`);
-})
+app.use("/api/chat",chatRoutes);
+
+const startServer = async () => {
+    await connectDB();
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+};
+
+startServer();
